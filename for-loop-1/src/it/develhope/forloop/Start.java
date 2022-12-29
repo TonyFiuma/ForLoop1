19194a0b2cb1c5c698b8a8e1194bcaa3d3a00168
Antonio Fiumano <113920158+TonyFiuma@users.noreply.github.com>
2022-12-29 22:20:08 +0300

    Add files via upload

---
+++ for-loop-1/src/it/develhope/forloop/Start.java
@@ -0,0 +1,10 @@
+package it.develhope.forloop;
+
+
+public class Start {
+    public static void main(String[] args) {
+        for(int i = 0; i<=25;i+=5){
+            System.out.println(i);
+        }
+    }
+}
